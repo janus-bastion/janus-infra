344ca50dc97947875e88f4ff1de5d503c7128bfb
Nathan Martel <nathan.martel@etu.mines-ales.fr>
2025-04-08 15:09:11 +0200

    feat: add table remote connection #1

--- janus-db/init-janus-db.sql
+++ janus-db/init-janus-db.sql
@@ -10,4 +10,28 @@ CREATE TABLE IF NOT EXISTS users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO users (username, email, password) VALUES ('janusadmin', 'janusadmin@example.com', 'janusadmin');
+-- Insérer l'utilisateur admin avec mot de passe hashé
+INSERT INTO users (username, email, password) 
+VALUES (
+    'janusadmin', 
+    'janusadmin@janus.fr', 
+    '$2y$12$VvxSEERinoO09Q1mQb8TguQKkavfjxj4PZbo.nbNr0gahGyIp/VEm'
+)
+ON DUPLICATE KEY UPDATE 
+    email = VALUES(email),
+    password = VALUES(password);
+
+CREATE TABLE remote_connections (
+    id int NOT NULL AUTO_INCREMENT,
+    user_id int NOT NULL,
+    name varchar(100) NOT NULL,
+    protocol enum('ssh','vnc','rdp') NOT NULL,
+    host varchar(255) NOT NULL COMMENT 'IP ou hostname',
+    port int DEFAULT NULL,
+    username varchar(100) NOT NULL,
+    password varchar(255) DEFAULT NULL,
+    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
