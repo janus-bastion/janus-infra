5371f655d59e5158bcce5c054d2d65cdc546f5da
Nathan Martel <nathan.martel@etu.mines-ales.fr>
2025-04-23 22:39:54 +0200

    feat: add script started tables #4

--- janus-db/init-janus-db.sql
+++ janus-db/init-janus-db.sql
@@ -10,28 +10,22 @@ CREATE TABLE IF NOT EXISTS users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- Insérer l'utilisateur admin avec mot de passe hashé
-INSERT INTO users (username, email, password) 
+INSERT IGNORE INTO users (username, email, password)
 VALUES (
-    'janusadmin', 
-    'janusadmin@janus.fr', 
+    'janusadmin',
+    'janusadmin@janus.fr',
     '$2y$12$VvxSEERinoO09Q1mQb8TguQKkavfjxj4PZbo.nbNr0gahGyIp/VEm'
-)
-ON DUPLICATE KEY UPDATE 
-    email = VALUES(email),
-    password = VALUES(password);
+);
 
-CREATE TABLE remote_connections (
-    id int NOT NULL AUTO_INCREMENT,
-    user_id int NOT NULL,
-    name varchar(100) NOT NULL,
-    protocol enum('ssh','vnc','rdp') NOT NULL,
-    host varchar(255) NOT NULL COMMENT 'IP ou hostname',
-    port int DEFAULT NULL,
-    username varchar(100) NOT NULL,
-    password varchar(255) DEFAULT NULL,
-    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (id),
-    KEY `user_id` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE IF NOT EXISTS remote_connections (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    protocol ENUM('ssh', 'vnc', 'rdp') NOT NULL,
+    host VARCHAR(255) NOT NULL,
+    port INT,
+    username VARCHAR(100) NOT NULL,
+    password VARCHAR(255),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+);
 
